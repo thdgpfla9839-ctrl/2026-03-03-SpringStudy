3c5ddd903f3bd8fbc638d8464c06c657d3e103d9
thdgpfla9839-ctrl <thdgpfla9839@gmail.com>
2026-08-03 10:59:36 +0900

    스프링 di 실습3

---
+++ SpringDIProject_3/src/main/java/com/sist/main/ClassPathXmlApplicationContext.java
@@ -0,0 +1,31 @@
+package com.sist.main;
+import java.util.*;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import java.io.*;
+// DI => 积己 (按眉 积己 => 蔼 林涝 => 按眉 家戈)
+// AOP
+public class ClassPathXmlApplicationContext 
+implements ApplicationContext
+{
+    private Map clsMap=new HashMap();
+    public ClassPathXmlApplicationContext(String path)
+    {
+    	try
+    	{
+    		SAXParserFactory spf=SAXParserFactory.newInstance();
+    		SAXParser sp=spf.newSAXParser();
+    		XMLParse xp=new XMLParse();
+    		sp.parse(new File(path), xp);
+    		clsMap=xp.getMap();
+    	}catch(Exception ex){}
+    }
+	@Override
+	public Object getBean(String key) {
+		// TODO Auto-generated method stub
+		return clsMap.get(key);
+	}
+
+}
